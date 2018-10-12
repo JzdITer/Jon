e6476e580ca8f98dab8202f068a157080e1beeb4
Jzd <651614883@qq.com>
2018-10-12 17:33:24 +0800

    创建JBarUtil

---
+++ library/src/main/java/com/jzd/android/jon/utils/JBarUtil.kt
@@ -0,0 +1,22 @@
+package com.jzd.android.jon.utils
+
+import android.content.Context
+
+class JBarUtil
+{
+    /**
+     * 获取状态栏高度
+     */
+    fun getStatusBarHeight(context: Context): Int
+    {
+        var result = 0
+        val resourceId = context.resources
+                .getIdentifier("status_bar_height", "dimen", "android")
+        if(resourceId > 0)
+        {
+            result = context.resources
+                    .getDimensionPixelSize(resourceId)
+        }
+        return result
+    }
+}
--- library/src/main/java/com/jzd/android/jon/widget/jtag/JTagView.kt
+++ library/src/main/java/com/jzd/android/jon/widget/jtag/JTagView.kt
@@ -3,7 +3,7 @@ package com.jzd.android.jon.widget.jtag
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.TextView
-
+//TODO 未完成，延后
 class JTagView(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : TextView(context, attrs, defStyleAttr)
 {
     constructor(context: Context?, attrs: AttributeSet?) : this(context, attrs, 0)

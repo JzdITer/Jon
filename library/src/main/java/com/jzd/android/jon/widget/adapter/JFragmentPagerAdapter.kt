01db350a2ee47e00957e08c41a1b5979267be586
Jzd <651614883@qq.com>
2018-12-11 13:52:53 +0800

    open

--- library/src/main/java/com/jzd/android/jon/widget/adapter/JFragmentPagerAdapter.kt
+++ library/src/main/java/com/jzd/android/jon/widget/adapter/JFragmentPagerAdapter.kt
@@ -4,7 +4,7 @@ import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 
-class JFragmentPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm)
+open class JFragmentPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm)
 {
 
     private var mFragments = arrayListOf<Fragment>()

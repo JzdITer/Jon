c4458e1fde79fb696dae86d306e9fce7a86b59e0
Jzd <651614883@qq.com>
2019-02-14 19:01:06 +0800

    提交JImagePager

---
+++ library/src/main/java/com/jzd/android/jon/widget/imgpager/JImagePager.kt
@@ -0,0 +1,12 @@
+package com.jzd.android.jon.widget.imgpager
+
+import android.content.Context
+import android.util.AttributeSet
+import com.jzd.android.jon.widget.viewpager.JViewPager
+
+class JImagePager(context: Context?, attrs: AttributeSet?) : JViewPager(context, attrs)
+{
+    constructor(context: Context?) : this(context, null)
+
+
+}
---
+++ library/src/main/java/com/jzd/android/jon/widget/imgpager/JImagePagerActivity.kt
@@ -0,0 +1,15 @@
+package com.jzd.android.jon.widget.imgpager
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.jzd.android.jon.R
+
+class JImagePagerActivity : AppCompatActivity()
+{
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_jimage_pager)
+    }
+}
